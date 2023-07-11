e3b762e08f0691703b7a318ae633b68b44d42ae4
psg9790 <psg9790@gmail.com>
2023-07-11 23:01:30 +0900

    event

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,64 +5,55 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <Windows.h>
 
-class SpinLock {
-public:
-	void lock() {
-		//한번에 동작하게끔 유도해주어야 함
-		//CAS (Compare-And-Swap)
-
-		bool expected = false;
-		bool desired = true;
-
-		//CAS 의사 코드
-		/*if (_locked == expected) {
-			expected = _locked;	//expected가 _locked로 변경되는 것에 주의
-			_locked = desired;
-			return true;
-		}
-		else 
-		{
-			return flase;
-		}*/
-
-		while (_locked.compare_exchange_strong(expected, desired) == false) {
-			expected = false;	//CPU 점유율 증가
-
-			this_thread::sleep_for(std::chrono::milliseconds(100));	//sleep, user모드와 kernel모드를 왕복하기 때문에 성능소모, 100ms 동안 스케줄링에 포함되지 않음
-			//this_thread::yield();	//언제든지 다시 스케줄링이 될 수 있지만, 현재는 1차적으로 반환해줌
-		}
-	}
-	void unlock() {
-		_locked.store(false);
-	}
-private:
-	atomic<bool> _locked = false;
-};
-
-int32 sum = 0;
 mutex m;
-SpinLock spinLock;
+queue<int32> q;
+HANDLE handle;
 
-void Add() {
-	for (int32 i = 0; i < 100'000;i++) {
-		lock_guard<SpinLock> guard(spinLock);
-		sum++;
+void Producer() {
+	while (true) {
+		unique_lock<mutex> lock(m);
+		q.push(100);
+
+		::SetEvent(handle); // Signal = true
+
+		this_thread::sleep_for(100ms);
 	}
-}void Sub() {
-	for (int32 i = 0; i < 100'000;i++) {
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
+}
+
+void Consumer() {
+	while (true) {
+		::WaitForSingleObject(handle, INFINITE);	//handle의 Signal 대기
+		//Non-Signal일 때 자동으로 Signal 초기화가 안되고 수동으로 해줘야함
+		//::ResetEvent(handle);
+
+		unique_lock<mutex> lock(m);
+		if (q.empty() == false) {
+			int32 data = q.front();
+			q.pop();
+			cout << data << '\n';
+		}
+		else {
+			cout << "none" << '\n';
+		}
+		
 	}
 }
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
+	//커널 오브젝트 - 유저~커널모드의 개입이 있기 때문에 사용성은 좋음, 하지만 추가적인 비용이 필요함
+	//Usage Count
+	//Signal / Non-Signal <<- bool
+	//Auto / Manual <<- bool
+	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*ManualReset*/, FALSE/*InitailState*/, NULL);
+
+	thread t1(Producer);
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << '\n';
+	::CloseHandle(handle);
 }

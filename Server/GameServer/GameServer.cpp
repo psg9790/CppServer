921c749e8e833c7bb09bf28da233ccbee3351c15
psg9790 <psg9790@gmail.com>
2023-07-12 15:00:28 +0900

    future (promise, packaged_task)

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,58 +6,96 @@
 #include <atomic>
 #include <mutex>
 #include <Windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-//참고) CV는 User-Level Object (커널 오브젝트 X)
-condition_variable cv;
-
-void Producer() {
-	while (true) {
-		//1) Lock을 잡고
-		//2) 공유 변수 값을 수정
-		//3) Lock을 풀고,
-		//4) 조건변수(condition_variable) 통해 다른 쓰레드에게 통지
-
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
-		// 여기 나오면 lock 파괴되는듯?
-		
-		cv.notify_one(); //wait중인 쓰레드 중에서 딱 1개를 깨운다
-
-		this_thread::sleep_for(100ms);
+int64 Calculate()
+{
+	int64 sum = 0;
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		sum += i;
 	}
+	return sum;
 }
 
-void Consumer() {
-	while (true) {
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() {return q.empty() == false;});	//내부적으로 조건을 충족하지 않으면 일시적으로 unlock 한 뒤, notify를 대기함: 
-		// notify 수신 시 lock을 다시 걸고 조건을 확인하는 절차를 밟을 것으로 예상
+void PromiseWorker(std::promise<string>&& promise)
+{
+	promise.set_value("secret message");
+}
 
-		//1) Lock을 잡고
-		//2) 조건 확인
-		//-만족0 => 빠져 나와서 이어서 코드를 진행
-		//-만족x => Lock을 풀어주고 대기 상태 (풀어줘야 하기 때문에 unique_lock 사용)
-
-		//while (q.empty() == false) 
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << q.size() << '\n';
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task) 
+{
+	task();
 }
 
 int main()
 {
-	thread t1(Producer);
-	thread t2(Consumer);
+	//동기(synchronous) 실행
+	int64 sum = Calculate();
+	cout << sum << '\n';
 
-	t1.join();
-	t2.join();
+	
+	//std::future
+	{
+		//1) deferred -> lazy evaluation 지연해서 실행하세요
+		//2) async -> 별도의 쓰레드를 만들어서 실행하세요
+		//3) deferred | async -> 둘 중 알아서 골라주세요
+
+		//언젠가 미래에 결과물을 뱉어줄거야!
+		std::future<int64> future = std::async(std::launch::async, Calculate);	//코루틴이랑 비슷?
+
+		//TODO
+		/*future_status status = future.wait_for(1ms);
+		if (status == future_status::ready)
+		{
+
+		}*/
+
+		int64 sum = future.get();	//결과물이 이제서야 필요하다!
+
+		/*class Knight {
+		public:
+			int64 getHp() { return 100; }
+		};
+		Knight knight;
+		std::future<int64> future2 = std::async(std::launch::async, Knight::getHp, knight);*/
+	}
+
+	//std::promise
+	{
+		//미래(future)에 결과물을 반환해줄거라 약속(std::promise) 해줘~ (계약서?)
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, std::move(promise));
+
+		string str = future.get();
+		cout << str << '\n';
+
+		t.join();
+	}
+
+	//std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << '\n';
+
+		t.join();
+	}
+
+	//결론)
+	//mutex, condition_variable까지 가지 않고 단순한 애들을 처리할 수 있는
+	//특히나, 한번 발생하는 단발성 이벤트에 유용하다.
+	//닭 잡는데 소 잡는 칼을 쓸 필요 없다.
+	//1) async
+	//원하는 함수를 비동기적으로 실행
+	//2) promise
+	//결과물을 promise를 통해 future로 받아줌
+	//3) packaged_task
+	//원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
 }

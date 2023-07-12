0f7b7e2c5d9f24761fa1164ef092a1ebf18748fa
psg9790 <psg9790@gmail.com>
2023-07-12 16:16:05 +0900

    pipeline

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -8,40 +8,55 @@
 #include <Windows.h>
 #include <future>
 
-int32 buffer[10'000][10'000];
+//가시성, 코드 재배치
+//가시성: 각 코어마다 다른 캐시를 사용하기 때문에 발생
+//코드 재배치: 컴파일러나 CPU의 자체 판단에 따라 코드 순서가 뒤바뀔 수도 있음
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
+
+volatile bool ready;
+
+void thread_1()
+{
+	while (!ready) {
+
+	}
+	//r1 = x;
+	y = 1;
+	r1 = x;
+}
+
+void thread_2()
+{
+	while (!ready) {
+
+	}
+	//r2 = y;
+	x = 1;
+	r2 = y;
+}
 
 int main()
 {
-	memset(buffer, 0, sizeof(buffer));
-
-	//faster (cache hit이 빈번하게 일어남)
+	int32 count = 0;
+	while (true)
 	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-		for (int32 i = 0; i < 10'000; i++)
-		{
-			for (int32 j = 0; j < 10'000; j++)
-			{
-				sum += buffer[i][j];
-			}
-		}
-		uint64 end = GetTickCount64();
+		ready = false;
+		count++;
+		x = y = r1 = r2 = 0;
+		thread t1(thread_1);
+		thread t2(thread_2);
 
-		cout << "Elapsed Tick: " << (end - start) << '\n';
-	}
-	//slower
-	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-		for (int32 i = 0; i < 10'000; i++)
-		{
-			for (int32 j = 0; j < 10'000; j++)
-			{
-				sum += buffer[j][i];
-			}
-		}
-		uint64 end = GetTickCount64();
+		ready = true;
 
-		cout << "Elapsed Tick: " << (end - start) << '\n';
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0) {
+			break;
+		}
 	}
+	cout << count << "번만에 빠져나옴" << '\n';
 }
